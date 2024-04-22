c4e10abd7ead628356d12ea29e03aef8d461ab4b
Paweu <pawelkruczkowski00@gmail.com>
2024-04-22 15:06:30 +0200

    .

---
+++ Pakiet4/06.funced_list.py
@@ -0,0 +1,11 @@
+def double(elem):
+    return elem+elem
+
+def applyToAll(lista, func):
+    newList = []
+    for elem in lista:
+        newList.append(func(elem))
+    return newList
+
+
+print(applyToAll([1,2,3,4,5,6,7,8,9, 'a'], lambda elem: elem + elem))
